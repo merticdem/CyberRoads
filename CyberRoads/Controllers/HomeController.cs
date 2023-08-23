25aed007c97f3883878548fa4af1627d0d20ee24
merticdem <merticdem@hotmail.com>
2023-08-23 12:43:19 +0300

    sa
    sa

--- CyberRoads/Controllers/HomeController.cs
+++ CyberRoads/Controllers/HomeController.cs
@@ -18,6 +18,10 @@ namespace CyberRoads.Controllers
         {
             return View();
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
