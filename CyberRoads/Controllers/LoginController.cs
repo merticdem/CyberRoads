7e4b5e0562947096abf5c96462c99f3ae098fe95
merticdem <merticdem@hotmail.com>
2023-09-04 02:06:12 +0300

    Last Update
    d

---
+++ CyberRoads/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CyberRoads.Controllers
+{
+
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public LoginController(SignInManager<AppUser> signInManager,
+            UserManager<AppUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+       
+        [HttpGet]
+        public ActionResult LoginS()
+        {
+            return View();
+        }
+    }
+}
